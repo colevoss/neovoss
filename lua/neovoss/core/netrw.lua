a24f4723b2b08c11d623b4107f1a4a1fd4288350
Cole Voss <voss.cole@gmail.com>
2024-01-06 18:52:03 -0600

    netrw remaps

--- lua/neovoss/core/netrw.lua
+++ lua/neovoss/core/netrw.lua
@@ -1,5 +1,18 @@
 local M = {}
 
+local group = vim.api.nvim_create_augroup("NeoVoss", { clear = true })
+vim.api.nvim_create_autocmd("FileType", {
+  group = group,
+  pattern = "netrw",
+  callback = function(ev)
+    local opts = { buffer = ev.buf, remap = true }
+
+    vim.keymap.set('n', 'l', '<CR>', opts)
+    vim.keymap.set('n', 'h', '-', opts)
+    vim.keymap.set('n', 'a', '%', opts)
+  end
+})
+
 M.setup = function()
   vim.g.netrw_banner = 1 -- 0 to disable
   -- vim.g.netrw_browse_split = 4
@@ -9,7 +22,6 @@ M.setup = function()
   vim.keymap.set("n", "<leader>eo", ":Explore<CR>")
   -- Open in vertical buffer
   vim.keymap.set("n", "<leader>el", ":Lexplore<CR>")
-
   vim.keymap.set("n", "<leader>er", ":Nexplore<CR>")
 end
 
